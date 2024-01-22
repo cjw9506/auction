6fda7b470ac35ec607e8104db6cfd30a41955a69
jeongjiwon <jungkt0x@naver.com>
2024-01-22 22:48:20 +0900

    feat: add chat repository

---
+++ src/main/java/com/auction/chat/repository/ChatRepository.java
@@ -0,0 +1,9 @@
+package com.auction.chat.repository;
+
+import com.auction.chat.domain.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
+}
